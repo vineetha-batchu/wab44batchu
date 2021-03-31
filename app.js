96d19a2adc368d79e80960f145630182077f6728
vineetha-batchu <77755310+vineetha-batchu@users.noreply.github.com>
2021-03-30 22:59:14 -0500

    jeeps view and route added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var jeepsRouter = require('./routes/jeeps')
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,7 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/jeeps',jeepsRouter)
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
